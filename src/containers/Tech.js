e8346ed8ae7fa8d7d05e1d54c910b5beb2766b72
La Fountain <github email address>
2019-04-27 09:18:29 -0400

    clean up file for new branch

---
+++ src/containers/Tech.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Tech extends Component {
+  constructor() {
+    super();
+    this.state = {
+      techskills: [
+        {
+          symbols: "fab fa-html5",
+          name: "HTML",
+          years: "3",
+          experience: "average"
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+      ]
+    };
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default Tech;
